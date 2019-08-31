94ecdf9fd619fad07054a5cccc44c8058f080149
Haowu <840291610@qq.com>
2019-08-31 23:29:26 +0800

    modify storage

--- utils/fdfs/storage.py
+++ utils/fdfs/storage.py
@@ -4,9 +4,8 @@ from fdfs_client.client import Fdfs_client
 
 
 class FDFSStorage(Storage):
-    '''fast dfs文件存储类'''
+    '''fastdfs文件存储类'''
     def __init__(self, client_conf=None, base_url=None):
-        '''初始化'''
         if client_conf is None:
             client_conf = settings.FDFS_CLIENT_CONF
         self.client_conf = client_conf
@@ -16,18 +15,17 @@ class FDFSStorage(Storage):
         self.base_url = base_url
 
     def _open(self, name, mode='rb'):
-        '''打开文件时使用'''
         pass
 
     def _save(self, name, content):
         '''保存文件时使用'''
-        # name:你选择上传文件的名字 test.jpg
+        # name:你选择上传文件的名字,如：test.jpg
         # content:包含你上传文件内容的File对象
 
         # 创建一个Fdfs_client对象
         client = Fdfs_client(self.client_conf)
 
-        # 上传文件到fast dfs系统中
+        # 上传文件到fastdfs系统中
         res = client.upload_by_buffer(content.read())
 
         # dict
