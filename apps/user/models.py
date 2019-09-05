b50b42ed40a29591c6761558d88c270016b2873f
Haowu <840291610@qq.com>
2019-09-05 23:44:31 +0800

    modify models

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from db.base_model import BaseModel
-# Create your models here.
 
 
 class User(AbstractUser, BaseModel):
